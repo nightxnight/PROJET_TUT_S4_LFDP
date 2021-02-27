0f050bc99556dddb2338b56a7da6db13a922e969
medkan01 <mehdiakniou@gmail.com>
2021-02-27 19:44:40 +0100

    types + fonctions

---
+++ main.ml
@@ -0,0 +1,18 @@
+type domino = D of int * int;; (*Type domino, qui représente un domino.*)
+
+type chain = E | S of int * string * int;; (*Type chain, qui représente la partie en cours. E si la partie démarre et S sinon*)
+
+type player = H of int | B of int;; (*Type player, qui représente un joueur. H pour un Homme et B pour un Bot.*)
+
+let flip (D(a,b)) = (D(b,a));; 
+
+let dominoToString (D(a, b)) = string_of_int a ^ "-" ^ string_of_int b;;
+
+let flip (D(a,b)) = (D(b,a));; (*Fonction flip qui retourne un domino.*)
+
+let append = function (*Fonction append qui ajoute un domino à la chaine*)
+  | (D(a,b), S(debut, str, fin), '<') -> (S(a, dominoToString (D(a,b)) ^ " " ^ str, fin))
+  | (D(a,b), S(debut, str, fin), '>') -> (S(debut, str ^ " " ^ dominoToString (D(a,b)), b))
+  | (D(a,b), E, '<') -> (S(a, dominoToString (D(a,b)), b))
+  | (D(a,b), E, '>') -> (S(a, dominoToString (D(a,b)), b))
+  | _ -> raise (Invalid_argument "Erreur dans l'utilisation de la fonction");;
