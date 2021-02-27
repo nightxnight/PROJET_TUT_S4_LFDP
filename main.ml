281b96e5a73b0d0b00ba8b8cc0be0696c926a212
medkan01 <mehdiakniou@gmail.com>
2021-02-27 20:35:50 +0100

    string_of_domino

--- main.ml
+++ main.ml
@@ -4,13 +4,13 @@ type chain = E | S of int * string * int;; (*Type chain, qui représente la part
 
 type player = H of int | B of int;; (*Type player, qui représente un joueur. H pour un Homme et B pour un Bot.*)
 
-let dominoToString (D(a, b)) = string_of_int a ^ "-" ^ string_of_int b;;
+let string_of_domino (D(a, b)) = string_of_int a ^ "-" ^ string_of_int b;;
 
 let flip (D(a,b)) = (D(b,a));; (*Fonction flip qui retourne un domino.*)
 
 let append = function (*Fonction append qui ajoute un domino à la chaine*)
-  | (D(a,b), S(debut, str, fin), '<') -> (S(a, dominoToString (D(a,b)) ^ " " ^ str, fin))
-  | (D(a,b), S(debut, str, fin), '>') -> (S(debut, str ^ " " ^ dominoToString (D(a,b)), b))
-  | (D(a,b), E, '<') -> (S(a, dominoToString (D(a,b)), b))
-  | (D(a,b), E, '>') -> (S(a, dominoToString (D(a,b)), b))
+  | (D(a,b), S(debut, str, fin), '<') -> (S(a, string_of_domino (D(a,b)) ^ " " ^ str, fin))
+  | (D(a,b), S(debut, str, fin), '>') -> (S(debut, str ^ " " ^ string_of_domino (D(a,b)), b))
+  | (D(a,b), E, '<') -> (S(a, string_of_domino (D(a,b)), b))
+  | (D(a,b), E, '>') -> (S(a, string_of_domino (D(a,b)), b))
   | _ -> raise (Invalid_argument "Erreur dans l'utilisation de la fonction");;
