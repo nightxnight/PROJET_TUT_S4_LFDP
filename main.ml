2007d61fbc911de6d7f1c6827e20af880c447ef2
medkan01 <mehdiakniou@gmail.com>
2021-03-03 11:19:49 +0100

    petite erreur de typage

--- main.ml
+++ main.ml
@@ -21,7 +21,6 @@ type chain = E | S of int * string * int;; (*Type chain, qui représente la part
 
 type player = H of int | B of int;; (*Type player, qui représente un joueur. H pour un Homme et B pour un Bot.*)
 
-
 (*
    _____           _   _                   _                                         _   __                        
   / ____|         | | (_)                 | |                                       | | /_/                        
@@ -33,13 +32,15 @@ type player = H of int | B of int;; (*Type player, qui représente un joueur. H 
                                                                         |_|                 |___/                  
 *)
 
+let string_of_domino (D(a, b)) = string_of_int a ^ "-" ^ string_of_int b;; (*Permet de récupérer la chaine de caractère d'un domino.*)
+
 let flip (D(a,b)) = (D(b,a));; (*Fonction flip qui retourne un domino.*)
 
 let append = function (*Fonction append qui ajoute un domino à la chaine*)
   | (D(a,b), S(debut, str, fin), '<') -> (S(a, string_of_domino (D(a,b)) ^ " " ^ str, fin))
   | (D(a,b), S(debut, str, fin), '>') -> (S(debut, str ^ " " ^ string_of_domino (D(a,b)), b))
   | (D(a,b), E, '<') -> (S(a, string_of_domino (D(a,b)), b))
-  | _ -> raise (Invalid_argument "Erreur dans l'utilisation de la fonction");;
+  | _ -> failwith "Erreur dans l'utilisation de la fonction";;
 
 let legal_adds (D(a, b)) chain =
   if chain = E then [append ((D(a, b)), chain, ' ')]
@@ -51,7 +52,6 @@ let legal_adds (D(a, b)) chain =
       | S(_, _, f) as chain when a=f -> [append (D(a, b), chain, '>')] 
       | _ -> []
     in List.concat [place (D(a, b)) chain;place (flip (D(a, b))) chain];;
-
 
 let possible_dominoes dominoes chain = (*Renvoie la liste de chacun des dominos d'une main donnée qui est plaçable au bout d'une chaîne donnée.*)
   if chain = E then dominoes
@@ -79,16 +79,15 @@ let rec suppress d = function
     | x::l when x = d || x = flip d -> l
     | x::l -> x::suppress d l;;
 
+let list = [];;
 
-let list = []
-let s = 0
+let s = 0;;
 
 (*input_move*)
 
 (*input_bot_move*)
 
 (*input_human_move*)
-
 
 (*
    _____           _   _                                         _  __  _             _ _                                     
@@ -104,23 +103,18 @@ let s = 0
 
 (*string_of_player*)
 
-let string_of_domino (D(a, b)) = string_of_int a ^ "-" ^ string_of_int b;; (*Permet de récupérer la chaine de caractère d'un domino.*)
-
 let rec take destination n source = 
 match (destination, n, source) with
 | (destination, 0, source) -> (destination, source)
 | (destination, n, []) -> (destination, [])
-| (destination, n, (D(a, b))::ts) -> take ((D(a, b))::destination) (n-1) ts
-;;
+| (destination, n, (D(a, b))::ts) -> take ((D(a, b))::destination) (n-1) ts;;
 
-(Version Waian) : 
+(*Version Waian*) 
 let rec string_of_dominoes = function
   | [] -> ""
   | D (a, b) :: l -> string_of_int a ^ "-" ^ string_of_int b ^ " " ^ string_of_dominoes l;;
 
 (*move*)
-
-
 
 (*
   __  __ _                               _                      _ _                                     _   _      
@@ -132,25 +126,25 @@ let rec string_of_dominoes = function
                                   | |                                                 | |                          
                                   |_|                                                 |_|                          
 *)
+
 let make_dominoes n = 
     let rec urs = function
     | (-1, -1) -> []
-    | (x,-1) -> usr (x-1, x-1)
-    | (x,y) -> (D(x,y))::usr (x, y-1)
-    in urs (n, n)
+    | (x,-1) -> urs (x-1, x-1)
+    | (x,y) -> (D(x,y))::urs (x, y-1)
+    in urs (n, n);;
 
-    
 let char_list_of_string c = 
   let rec urs s l =
     if s < 0 
     then l 
     else urs (s - 1) (c.[s] :: l) in urs (String.length c - 1) [];;
 
+let list_of_players = [];;
 
-let list_of_players = []
-let i = 0
+let i = 0;;
 
-let player_ofstring s = 
+let player_of_string s = 
   let rec urs i l =
     if i < 0 
     then l 
@@ -158,7 +152,7 @@ let player_ofstring s =
       match s.[i] with 
       |'H' -> urs (i - 1) (H (i+1) :: l) 
       |'B' -> urs (i - 1) (B (i+1) :: l) 
-      | -> failwith "erreur"
+      | _ -> failwith "erreur"
       in urs (String.length s - 1) [] ;;
       
 (*get_hand_size*)
@@ -175,15 +169,16 @@ let player_ofstring s =
                      | |             | |                                                    
                      |_|             |_|                                                    
 *)
-  let string_of_chain = function (*Renvoie la chaine représentant la partie en cours*)
-    |E -> ""
-    |S (nb_g, str, nb_d) -> str
 
-  let string_of_state l = function (*Renvoie l'état d'un joueur*)
-    |H(x) when l = [] -> "Joueur "^x^" (humain) : \t"
-    |H(x)-> "Joueur "^x^" (humain) : \t"^string_of_dominoes(l)
-    |B(x) when l = [] -> "Joueur "^x^" (bot) : \t"
-    |B(x)-> "Joueur "^x^" (bot) : \t"^string_of_dominoes(l) 
+let string_of_chain = function (*Renvoie la chaine représentant la partie en cours*)
+  | E -> ""
+  | S (nb_g, str, nb_d) -> str;;
+
+let string_of_state l = function (*Renvoie l'état d'un joueur*)
+  | H x when l = [] -> "Joueur " ^ string_of_int x ^" (humain) : \t"
+  | H x -> "Joueur " ^ string_of_int x ^" (humain) : \t"^string_of_dominoes(l)
+  | B x  when l = [] -> "Joueur " ^ string_of_int x ^" (bot) : \t"
+  | B x -> "Joueur " ^ string_of_int x ^ " (bot) : \t"^string_of_dominoes(l) ;;
 
 (*list_shuffle*)
 
